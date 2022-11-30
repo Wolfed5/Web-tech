6b88c296c53d58f9f055ca0ddfff8cd75ee2ea5e
Wolfed5 <89199186+Wolfed5@users.noreply.github.com>
2022-11-30 14:15:18 +0300

    Add files via upload

--- WT/script2.js
+++ WT/script2.js
@@ -1,109 +1,49 @@
-count = 5; // количество изображений на  web-странице
-
-pics = new Array(5);
-
-function preload() { 
-
-  // упреждающая загрузка 5-ти изображений и размещение их в массиве
-
-for (i=0; i<count; i++) pics[i] = new Image();
-
-    pics[0].src = "step.bmp";
-
-    pics[1].src = "scn.bmp";
-
-    pics[2].src = "cdb.bmp";
-
-    pics[3].src = "cdb.bmp";
-
-    pics[4].src = "cdb.bmp";
-
-
-document.images[0].src = pics[1].src;
-}
-function up()
-
+function getimg()
 {
-
-document.mypic.src="step.bmp"
-
+    $tmp= document.forms[0].template.value;
+    document.img1.src= $tmp + ".bmp";
+    
 }
 
-function down()
-
-{
-
-document.mypic.src="cdb.bmp"
-
-}
 
 bool = 0;
 var idtimer;
-delay = 1000;
+delay = 3000;
 function animation()
 { 
- //   for(i=0;i>-1;i++)
- //   {
     if(bool == 0)
     {
-        document.img1.src = "http://placehold.it/300x300?text=2"
+        document.img1.src = "step.bmp"
         bool = 1;
     }
     else if(bool==1)
     {
-        document.img1.src = "http://placehold.it/300x300?text=3"
+        document.img1.src = "scn.bmp"
         bool = 2;
     }
     else if(bool==2)
     {
-        document.img1.src = "http://placehold.it/300x300?text=4"
-        bool = 3;
-    }
-    else if(bool==3)
-    {
-        document.img1.src = "http://placehold.it/300x300?text=5"
-        bool = 4;
-    }
-    else if(bool==4)
-    {
-        document.img1.src = "http://placehold.it/300x300?text=6"
-        bool = 5;
-    }
-    else if(bool==5)
-    {
-        document.img1.src = "http://placehold.it/300x300?text=7"
-        bool = 6;
-    }
-    else if(bool==6)
-    {
-        document.img1.src = "http://placehold.it/300x300?text=8"
-        bool = 7;
-    }
-    else if(bool==7)
-    {
-        document.img1.src = "http://placehold.it/300x300?text=9"
-        bool = 8;
-    }
-    else if(bool==8)
-    {
-        document.img1.src = "http://placehold.it/300x300?text=10"
-        bool = 9;
-    }
-    else if(bool==9)
-    {
-        document.img1.src = "http://placehold.it/300x300?text=11"
-        bool = 10;
-    }
-    else if(bool==10)
-    {
-        document.img1.src = "http://placehold.it/300x300?text=12"
-        bool = 11;
-    }
-    else if(bool==11)
-    {
-        document.img1.src = "http://placehold.it/300x300?text=1"
+        document.img1.src = "cdb.bmp"
         bool = 0;
     }
     idtimer = setTimeout('animation()', delay);
-    }
-//}
+}
+
+
+pictures = new Array();
+pictures[1].src="step.bmp";
+pictures[2].src="scn.bmp";
+pictures[3].src="cdb.bmp";
+for(i=0; i<4; i++)
+ {
+   pictures[i] = new Image();
+   if(i==1) pictures[i].src = "step.bmp";
+   if(i==2) pictures[i].src = "scn.bmp";
+   if(i==3) pictures[i].src = "cdb.bmp";
+ }
+
+function LoadImg() {
+  document.images[0].src =
+     pictures[document.form1.item.selectedIndex].src;
+     bool=document.form1.item.selectedIndex;
+}
